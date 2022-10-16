f2cd574333cbbd1c15f0dc46f896b0c7ee375eb0
demestor <mickhaildusha@gmail.com>
2022-10-16 17:31:20 +0300

    Версия первая

---
+++ main.py
@@ -0,0 +1,11 @@
+from tesr_2 import a_m_2_2
+from tesr_2 import size1
+
+print("Режимы работы:\n Опредилитель - 1; \n Сложение - 2")
+in_work = int(input("Выберите режим: "))
+if in_work == 1:
+    size1()
+    
+if in_work == 2:
+    print("В разработке.")
+    #size()
